68410fdce7a14315f7e459e3b166a14e00dc4b2f
StotTot <a.debride.2016@gmail.com>
2025-06-29 18:27:22 -0500

    new

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/StotTot.github.io/Word-Game/",
   plugins: [react()],
     build: {
     outDir: './build'
